07ae7a7a5e0fbc2de2761b5d1ddc5df54cdb16a3
rain7night <rainnight@outlook.com>
2018-01-26 14:17:28 +0800

    update ip for binding linux port

--- src/main/java/server/DemoServer.java
+++ src/main/java/server/DemoServer.java
@@ -8,13 +8,13 @@ public class DemoServer {
     private static Logger logger = Logger.getLogger(DemoServer.class);
 
     public static void main(String[] args) {
-        String addressFind = "http://localhost:7070/webservice/UpdateService";
+        String addressFind = "http://0.0.0.0:7070/webservice/UpdateService";
         UpdateService service = new UpdateServiceImpl();
 
         Endpoint.publish(addressFind,service);
         logger.info("Server UpdateService start up ! WebService is published !");
 
-        String addressUpdate = "http://localhost:7070/webservice/FindService";
+        String addressUpdate = "http://0.0.0.0:7070/webservice/FindService";
         FindService serviceUpdate = new FindServiceImpl();
 
         Endpoint.publish(addressUpdate,serviceUpdate);
